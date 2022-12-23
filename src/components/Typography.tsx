c8c605df04aebec981672b8a4e2032fabc8067ca
Raman Halder <ramanhalder12@gmail.com>
2022-12-23 13:18:17 +0530

    --added-typography-components

---
+++ src/components/Typography.tsx
@@ -0,0 +1,59 @@
+import { Text } from "@/atoms"
+import { Theme } from "@/themes"
+import { TextProps } from "@shopify/restyle"
+import React from "react"
+
+interface Props {
+    props?: TextProps<Theme>,
+    children: React.ReactNode
+}
+
+export const RegularText: React.FC<Props> = ({ props, children }) => {
+    return (
+        <>
+            <Text fontFamily={'Comfortaa-Regular'} {...props}>
+                {children}
+            </Text>
+        </>
+    )
+}
+
+export const BoldText: React.FC<Props> = ({ props, children }) => {
+    return (
+        <>
+            <Text fontFamily={'Comfortaa-Bold'} {...props}>
+                {children}
+            </Text>
+        </>
+    )
+}
+
+export const MediumText: React.FC<Props> = ({ props, children }) => {
+    return (
+        <>
+            <Text fontFamily={'Comfortaa-Medium'} {...props}>
+                {children}
+            </Text>
+        </>
+    )
+}
+
+export const SemiBoldText: React.FC<Props> = ({ props, children }) => {
+    return (
+        <>
+            <Text fontFamily={'Comfortaa-SemiBold'} {...props}>
+                {children}
+            </Text>
+        </>
+    )
+}
+
+export const lightText: React.FC<Props> = ({ props, children }) => {
+    return (
+        <>
+            <Text fontFamily={'Comfortaa-Light'} {...props}>
+                {children}
+            </Text>
+        </>
+    )
+}
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,16 +1,17 @@
-import { Box, Text } from '@/atoms'
+import { Box } from '@/atoms'
 import { DrawerContentComponentProps } from '@react-navigation/drawer'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { SemiBoldText } from './Typography'
 
 
 const Sidebar: React.FC<DrawerContentComponentProps> = () => {
     return (
         <Box flex={1} bg={'$sidebarBackground'}>
             <SafeAreaView>
-                <Text variant='sidebar' m='lg' fontFamily={'Comfortaa-Bold'}  fontSize={30}>
+                <SemiBoldText props={{ variant: 'sidebar', fontSize: 35, m: 'lg' }}>
                     leadistro
-                </Text>
+                </SemiBoldText>
             </SafeAreaView>
         </Box>
     )

811b5a6422648ad1d1f25d8a84873105dffa798e
Raman Halder <ramanhalder12@gmail.com>
2023-02-13 13:00:50 +0530

    --minor-sidebar-changes-update-needed-main

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,11 +1,12 @@
-import { Box, Pressable, TouchableOpacity } from '@/atoms'
+import { Box, Pressable } from '@/atoms'
 import { DrawerContentComponentProps } from '@react-navigation/drawer'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { BoldText, MediumText, SemiBoldText } from './Typography'
-import { Alert, Image } from 'react-native'
+import { BoldText } from './Typography'
+import { Alert, } from 'react-native'
 import FeatherIcon from './icon'
 import auth from '@react-native-firebase/auth'
+import Image from '@/atoms/image'
 // import { HomeDrawerParamList } from '@/navs'
 
 // TODO !! - Complete Navigation To Other Screens
@@ -16,53 +17,98 @@ const Sidebar: React.FC<DrawerContentComponentProps> = ({ navigation }) => {
     return (
         <Box flex={1} bg={'$sidebarBackground'}>
             <SafeAreaView>
-                <Image source={require('../assets/images/leadistroWhite.png')} style={{ maxHeight: 100, width: '100%', alignSelf: 'center', resizeMode: 'contain', marginTop: 25 }} />
-                <SemiBoldText fontName='Comfortaa' props={{ textAlign: 'center', variant: 'sidebar', fontSize: 35, m: 'lg' }}>
-                    leadistro
-                </SemiBoldText>
+                <Box flexDirection={'column'} alignItems={'center'} mt={'lg'} justifyContent={'flex-start'} px={'md'} width={'100%'}>
+                    <Image
+                        source={require('../assets/images/leadistroWhite.png')}
+                        height={75} width={75} borderRadius={'sm'}
+                        resizeMode={'contain'} resizeMethod='auto' alignItems={'center'} justifyContent={'center'}
+                    />
+                    <BoldText fontName='Comfortaa' props={{ textAlign: 'center', variant: 'sidebar', fontSize: 32.5, px: 'lg', py: 'lg' }}>
+                        leadistro
+                    </BoldText>
+                </Box>
                 <Box mt={"xl"} ml='lg' justifyContent={'space-between'} flexDirection={'column'} >
-                    <Pressable onPress={() => navigation.navigate('Emp')}>
+                    <Pressable
+                        flexDirection={'row'} width={'100%'} alignItems={'center'} justifyContent={'flex-start'}
+                        onPress={() => navigation.navigate('Emp')}>
+                        <FeatherIcon
+                            style={{ paddingRight: 15 }}
+                            name='search'
+                            size={20} />
                         <BoldText fontName='Comfortaa' props={{
                             variant: 'sidebar', fontSize: 18, my: 'xl', numberOfLines: 1,
                         }}>
-                            Research Cold Leads
+                            Research Leads
                         </BoldText>
                     </Pressable>
-                    <Pressable onPress={() => navigation.navigate('SSW')}>
+                    <Pressable
+                        flexDirection={'row'} width={'100%'} alignItems={'center'} justifyContent={'flex-start'}
+                        onPress={() => navigation.navigate('SSW')}>
+                        <FeatherIcon
+                            style={{ paddingRight: 15 }}
+                            name='server'
+                            size={20} />
                         <BoldText fontName='Comfortaa' props={{
                             variant: 'sidebar', fontSize: 18, my: 'xl', numberOfLines: 1,
                         }}>
-                            Search Similar Websites
+                            Search Similar SEO
                         </BoldText>
                     </Pressable>
-                    <Pressable onPress={() => navigation.navigate('Email')}>
+                    <Pressable
+                        flexDirection={'row'} width={'100%'} alignItems={'center'} justifyContent={'flex-start'}
+                        onPress={() => navigation.navigate('Email')}>
+                        <FeatherIcon
+                            style={{ paddingRight: 15 }}
+                            name='mail'
+                            size={20} />
                         <BoldText fontName='Comfortaa' props={{
                             variant: 'sidebar', fontSize: 18, my: 'xl', numberOfLines: 1,
                         }}>
-                            Email Marketing
+                            Send Custom Email
                         </BoldText>
                     </Pressable>
-                    <Pressable onPress={() => navigation.navigate('CreateContacts')}>
+                    <Pressable
+                        flexDirection={'row'} width={'100%'} alignItems={'center'} justifyContent={'flex-start'}
+                        onPress={() => navigation.navigate('CreateContacts')}>
+                        <FeatherIcon
+                            style={{ paddingRight: 15 }}
+                            name='user-plus'
+                            size={20} />
                         <BoldText fontName='Comfortaa' props={{
-                            variant: 'sidebar', fontSize: 18, my: 'xl', numberOfLines: 1,
+                            variant: 'sidebar', fontSize: 20, my: 'xl', numberOfLines: 1,
                         }}>
-                            Create Contact
+                            Create Contacts
                         </BoldText>
                     </Pressable>
-                    <Pressable onPress={() => navigation.navigate('Settings')}>
+                    <Pressable
+                        flexDirection={'row'} width={'100%'} alignItems={'center'} justifyContent={'flex-start'}
+                        onPress={() => navigation.navigate('Settings')}>
+                        <FeatherIcon
+                            style={{ paddingRight: 15 }}
+                            name='settings'
+                            size={20} />
                         <BoldText fontName='Comfortaa' props={{
-                            variant: 'sidebar', fontSize: 18, my: 'xl', numberOfLines: 1,
+                            variant: 'sidebar', fontSize: 20, my: 'xl', numberOfLines: 1,
                         }}>
                             Settings
                         </BoldText>
                     </Pressable>
+                    <Pressable
+                        flexDirection={'row'} width={'100%'} alignItems={'center'} justifyContent={'flex-start'} mt={'50'}
+                        onPress={() => {
+                            auth().signOut().then(() => Alert.alert('Signed Out', 'You Have been successfully signed out, We Will Update your Distro'))
+                        }}>
+                        <FeatherIcon
+                            style={{ paddingRight: 15 }}
+                            name='log-out'
+                            size={22.5} />
+                        <BoldText fontName='Poppins' props={{
+                            variant: 'sidebar', fontSize: 25, my: 'xl', numberOfLines: 1,
+                        }}>
+                            Log Out
+                        </BoldText>
+                    </Pressable>
                 </Box>
-                <TouchableOpacity onPress={() => {
-                    auth().signOut().then(() => Alert.alert('Signed Out', 'You Have been successfully signed out, We Will Update your Distro'))
-                }} mt='xl' mx={'lg'} flexDirection={'row'} width={'100%'} alignItems={'center'} justifyContent={'flex-start'} flex={1}>
-                    <FeatherIcon name='log-out' size={25} />
-                    <MediumText fontName='Poppins' props={{ variant: 'sidebar', fontSize: 20, mx: 'lg' }}>Log Out</MediumText>
-                </TouchableOpacity>
 
             </SafeAreaView>
         </Box>
--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,20 +1,31 @@
-import { Container } from '@/atoms'
-import { BoldText } from '@/components/Typography'
+import { Box, Container } from '@/atoms'
+import { BoldText, SemiBoldText } from '@/components/Typography'
 import { HomeDrawerParamList, RootStackParamList } from '@/navs'
 import { DrawerScreenProps } from '@react-navigation/drawer'
 import { CompositeScreenProps } from '@react-navigation/native'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import React from 'react'
+import auth from '@react-native-firebase/auth'
 
 type Props = CompositeScreenProps<DrawerScreenProps<HomeDrawerParamList, "Main">, NativeStackScreenProps<RootStackParamList>>
 
 
 export default function MainScreen({ navigation }: Props) {
+    const user = auth().currentUser
     return (
-        <Container justifyContent={'flex-start'} alignItems={'center'}>
-            <BoldText fontName='Poppins'>
-                Main Screen
-            </BoldText>
+        <Container justifyContent={'flex-start'} alignItems={'flex-start'} py={'md'} px={'md'} >
+            <Box flexDirection={'column'} width={'100%'} alignItems={'flex-start'} justifyContent={'center'}>
+                <BoldText fontName='Comfortaa' props={{
+                    fontSize: 27.5,
+                }}>
+                    Account
+                </BoldText>
+                <SemiBoldText fontName='Poppins' props={{
+                    fontSize: 25
+                }}>
+                    {user?.displayName}
+                </SemiBoldText>
+            </Box>
         </Container>
     )
 }

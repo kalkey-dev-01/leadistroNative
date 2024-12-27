23adf0c26590ce8f09b41d2496d07cfa7140f989
Raman Halder <87334927+MrJ0bs@users.noreply.github.com>
2024-12-27 11:09:21 +0530

    --rm

--- src/api/uri-with-keys.ts
+++ src/api/uri-with-keys.ts
@@ -1,8 +1,8 @@
 export const employeesApi =
-  'https://app.leadwity.com/api-product/incoming-webhook/enrich-company'
+  ''
 export const convertCompanyNamesApi =
-  'https://app.leadwity.com/api-product/incoming-webhook/convert-company-names'
+  ''
 export const fnlndApi =
-  'https://app.leadwity.com/api-product/incoming-webhook/find-emails-first-last'
-export const eefuApi = `https://app.leadwity.com/api-product/incoming-webhook/extract-emails-from-urls`;
-  export const apikey = 'U6D5E5D1-N4T9I4Y6-B3O5S9S0-Z5F5Z0N3'
+  ''
+export const eefuApi = ``;
+export const apikey = ''
